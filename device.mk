478a47f67aafd052c74be8fc3ef0f177465beaef
TH779 <i@779.moe>
2021-06-05 15:21:39 +0800

    lmi: Import seccomp policies
    * From V12.5.2.0.RJKCNXM.
    
    * Schnedi: Use find-copy-subdir-files
    
    Change-Id: I8b346fbb3c40b4b1d917a58373faeac067f49868

--- device.mk
+++ device.mk
@@ -287,6 +287,10 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/seccomp,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy) \
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
