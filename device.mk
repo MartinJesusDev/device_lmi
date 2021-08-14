dd159918823b968b6b5c2216ebd8a8bcfbbbb41d
TH779 <i@779.moe>
2021-08-14 12:47:36 +0800

    lmi: Import audio configs
    * From V12.5.2.0.RJKCNXM
    * Also remove QTI a2dp offload entry
    
    * Schnedi: Use find-copy-subdir-files
    
    Change-Id: Ia46df1a6ab6d73bee556369b52318dbcf20d7087

--- device.mk
+++ device.mk
@@ -75,6 +75,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio,$(TARGET_COPY_OUT_VENDOR)/etc) \
+    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/gps,$(TARGET_COPY_OUT_VENDOR)/etc)
