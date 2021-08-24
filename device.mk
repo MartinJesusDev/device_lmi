645f327b8d092fc4104a5cc3dbc0e1a4ec1a9543
TH779 <i@779.moe>
2021-08-24 13:04:10 +0800

    lmi: Import aidl QTI vibrator HAL
    Change-Id: I55828e7e445c4dc3d5382298dfec3c86d804bbd1

--- device.mk
+++ device.mk
@@ -336,7 +336,10 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.xiaomi_lmi
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vibrator/etc/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # WiFi
 PRODUCT_PACKAGES += \
