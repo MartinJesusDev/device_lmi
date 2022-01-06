fd96e70124f102c37a1ee3c723e2af04ce3cee47
TH779 <i@779.moe>
2022-01-06 10:15:34 +0100

    lmi: Initial skeleton tree
    Change-Id: I21b59a6c243d9466c94b8193138b6ab53aa6cbc2

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/titan_lmi.mk
+
+COMMON_LUNCH_CHOICES := \
+    titan_lmi-userdebug \
+    titan_lmi-eng
